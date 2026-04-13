2d9082295b6795eb43b9adaff9385b234b54aef6
davyybs <daviferreira828@gmail.com>
2026-04-12 22:03:22 -0300

    Creation of Database

---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, Column, Integer, Boolean, Float, String
+from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+db = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+#tabela de histórico
+class CashbackHistory(Base):
+    __tablename__ = "cashback_history"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    user_ip = Column("user_ip", String(255), nullable=False)
+    price = Column("price", Float, nullable=False)
+    discount = Column("discount", Integer, default=0)
+    vip = Column("vip", Boolean, nullable=False)
+    cashback_value = Column("cashback_value", Float, nullable=False)
+
+    def __init__(self, user_ip, price, discount, vip, cashback_value):
+        self.user_ip = user_ip
+        self.price = price
+        self.discount = discount
+        self.vip = vip
+        self.cashback_value = cashback_value
