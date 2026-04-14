a78db85669ba1cba55e414f2ca78793cae40b840
davyybs <daviferreira828@gmail.com>
2026-04-13 21:11:35 -0300

    Middleware added

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-from routes import router
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
