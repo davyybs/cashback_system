e405281a4425367439810c0b1eec92f3a204dd05
davyybs <daviferreira828@gmail.com>
2026-04-13 21:16:43 -0300

    IP Request, Commit and new route added

--- backend/routes.py
+++ backend/routes.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from models import CashbackHistory
 
 router = APIRouter(prefix="/cashback", tags=["cashback"])
 
 @router.get("/")
-async def calculate_cashback(price:float, vip:bool, discount:int = 0):
+async def calculate_cashback(request: Request, price:float, vip:bool, discount:int = 0, db: Session = Depends(get_db)):
     """
     Rota padrão para calcular o cashback da compra do cliente
     """
+    user_ip = request.client.host #get the user IP
+
     normalCashback = 0.05
     vipCashback = 0.1
 
@@ -21,5 +26,26 @@ async def calculate_cashback(price:float, vip:bool, discount:int = 0):
 
     if price > 500:
         cashbackValue *= 2
+
+    #create an object and add the values
+    registration = CashbackHistory(
+        user_ip=user_ip,
+        price=price,
+        discount=discount,
+        vip=vip,
+        cashback_value=cashbackValue
+    )
+
+    db.add(registration)
+    db.commit()
+    db.refresh(registration) 
     
-    return {"Valor do Cashback:": cashbackValue}
+    return {"cashback_value": cashbackValue, "id": registration.id}
+
+@router.get("/history")
+async def get_history(request: Request, db: Session = Depends(get_db)):
+    user_ip = request.client.host
+    
+    query = db.query(CashbackHistory).filter(CashbackHistory.user_ip == user_ip).all()
+    
+    return query
