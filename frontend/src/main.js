40444161b2928d68703c378225c07148da47fd2d
davyybs <daviferreira828@gmail.com>
2026-04-13 22:57:15 -0300

    API integration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,16 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+document.getElementById("cashbackForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+    
+    const price = document.getElementById("price").value;
+    const discount = document.getElementById("discount").value || 0;
+    const vip = document.getElementById("vip").value;
+    
+    const url = `http://localhost:8000/cashback/?price=${price}&discount=${discount}&vip=${vip}`;
+    
+    const answer = await fetch(url);
+    const data = await answer.json();
+    
+    document.getElementById("result").innerText= `Valor do Cashback: R$${data.cashback_value}`;
+});
