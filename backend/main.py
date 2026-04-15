8d8c6a2b1fbf11b32ffa4e02194f69e2126ddc6c
davyybs <daviferreira828@gmail.com>
2026-04-14 22:34:51 -0300

    Vercel integration

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["cashback-system-six.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
